1bab12c15de2db205caf1a5ffb73c29b63cad64e
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2025-05-30 19:23:58 -0400

    Remove profiler as it can be done externally

--- pianola/__main__.py
+++ pianola/__main__.py
@@ -1,13 +1,11 @@
 import sys
 
-from broken import BrokenProfiler
 from pianola import PianolaScene
 
 
 def main():
-    with BrokenProfiler("PIANOLA"):
-        pianola = PianolaScene()
-        pianola.cli(sys.argv[1:])
+    pianola = PianolaScene()
+    pianola.cli(sys.argv[1:])
 
 if __name__ == "__main__":
     main()
