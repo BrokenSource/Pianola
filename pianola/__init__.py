3f122664a0a6ad73b544f72b8d162afd3d1d5ef3
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2026-07-19 08:53:09 -0300

    speed: sync init metadata with a script rather than runtime importlib

--- pianola/__init__.py
+++ pianola/__init__.py
@@ -1,10 +1,9 @@
 from dearlog import logger  # isort: split
 
-from importlib.metadata import metadata
-
-__meta__    = metadata(str(__package__))
-__about__   = __meta__.get("Summary")
-__version__ = __meta__.get("Version")
+__about__   = "🎹 Smoothest piano roll visualizer"
+__package__ = "pianola"
+__version__ = "0.10.0"
+__license__ = "AGPL-3.0"
 
 from pathlib import Path
 
